b3b52eb9ab371d96dc774e6f07a694c501ca99f4
SelimMeske <mesic.selim@outlook.com>
2024-04-25 18:03:39 +0200

    Upgraded to java 17

--- app/src/main/java/vk/vaktija/view/CountDownTimer.kt
+++ app/src/main/java/vk/vaktija/view/CountDownTimer.kt
@@ -1,6 +1,5 @@
 package vk.vaktija.view
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
